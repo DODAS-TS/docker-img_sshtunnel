12ecded2f1f09a3f6af7ad81ed31643622208d93
MircoT <mirco.theone@gmail.com>
2018-07-18 11:45:05 +0200

    Add healthchecks

---
+++ check_ssh_server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from paramiko import (AuthenticationException, AutoAddPolicy,
+                      BadHostKeyException, SSHException)
+from paramiko.client import SSHClient
+from os import environ
+
+
+def main():
+    try:
+        client = SSHClient()
+        client.set_missing_host_key_policy(AutoAddPolicy)
+        client.connect("localhost", port=environ.get('TARGET_SSH_PORT', 31042),
+                       username="admin", password="passwd")
+    except (BadHostKeyException, AuthenticationException,
+            SSHException):
+        return 1
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ check_ssh_tunnel.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ZOOKEEPER_TARGET_PUB_KEY=$(dodas_cache --wait-for true zookeeper TARGET_PUB_KEY || echo "None")
+LOCAL_PUB_KEY=$(< /opt/dodas/keys/id_rsa.pub)
+
+if [ "$LOCAL_PUB_KEY" == "$ZOOKEEPER_TARGET_PUB_KEY" ];
+then
+    exit 0 ;
+else
+    exit 1 ;
+fi
