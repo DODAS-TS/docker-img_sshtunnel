2e7c4cc46a3a02d2b7347cd598e2940fa633b1f6
MircoT <mirco.theone@gmail.com>
2018-07-18 12:31:27 +0200

    Fix check tunnel as shell script

--- check_ssh_tunnel.sh
+++ check_ssh_tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ZOOKEEPER_TARGET_PUB_KEY=$(dodas_cache --wait-for true zookeeper TARGET_PUB_KEY || echo "None")
-LOCAL_PUB_KEY=$(< /opt/dodas/keys/id_rsa.pub)
+LOCAL_PUB_KEY=$(cat /opt/dodas/keys/id_rsa.pub)
 
 if [ "$LOCAL_PUB_KEY" == "$ZOOKEEPER_TARGET_PUB_KEY" ];
 then
